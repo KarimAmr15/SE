bd339928ea33c33af10203648c83cd973f76c982
Youssef <youssef.amr.04@gmail.com>
2024-05-13 18:41:34 +0300

    Components folder

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import react from 'react'
+
+const Header = () => {
+
+    return(
+        <div>
+            <h1 classname = "font-weight-light display-1 text-center"> Customers App</h1>
+        </div>
+    )
+
+}
+
+export default Header
