9c58238546a0c7dd54876dac58f32cc201784606
Youssef <youssef.amr.04@gmail.com>
2024-05-13 18:28:40 +0300

    jsx files and routing

---
+++ client/src/routes/CustDetails.jsx
@@ -0,0 +1,12 @@
+import react from 'react'
+
+const CustDetails = () =>
+    {
+        return(
+            <div>
+                CustDetails
+            </div>
+        )
+    }
+
+    export default CustDetails
